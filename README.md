b56c8579f100d1d423b775ef04b4de7af01cf47f
Jamshed Sobirov <jamshed_corp@mail.ru>
2022-10-19 08:59:42 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# freemarket
-Freelance Market
+# Freelancer_Marketplace
+Powerful Freelance Marketplace with ability to change the users
---
+++ app/includes/info.php
@@ -0,0 +1,6 @@
+<?php
+
+define('SOFTWARE_NAME', 'SkyNetSystems Service Platform');
+define('SOFTWARE_AUTHOR', 'Jamshed Sobirov');
+define('SOFTWARE_URL', 'https://skynetsystems.ru');
+define('SOFTWARE_VERSION', '1.0.0');
